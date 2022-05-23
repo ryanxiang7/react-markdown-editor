ed3f765537ba15373f8e3b4b1a13bbac97ba2e67
Ryan <995067931@qq.com>
2022-05-23 16:12:33 +0800

    ts declare

--- src/index.d.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { BTNTYPE } from './utils/var';
-import Editor, { use, unuse, getMDVal, getHTMLVal } from './editor';
-
-export { Editor, BTNTYPE, use, unuse, getMDVal, getHTMLVal };
