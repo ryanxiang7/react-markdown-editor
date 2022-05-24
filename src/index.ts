d836ac003f10bb00503214df6743179c3bded55f
Ryan <995067931@qq.com>
2022-05-24 12:21:53 +0800

    export BTNTYPE

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import Editor, { getEditorConfig, getMDVal, getHTMLVal, use, unuse } from './editor';
+
+import { BTNTYPE } from './utils/var';
+
+export default Editor;
+
+export { getEditorConfig, getHTMLVal, getMDVal, use, unuse, BTNTYPE };
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import requireTransform from 'vite-plugin-require-transform';
 const path = require('path');
 export default defineConfig({
+	// root: './demo',
 	css: {
 		postcss: {
 			plugins: [require('postcss-import'), require('tailwindcss'), require('autoprefixer')],
@@ -11,7 +12,7 @@ export default defineConfig({
 	plugins: [requireTransform({}), react()],
 	build: {
 		lib: {
-			entry: path.resolve(__dirname, 'src/editor.tsx'),
+			entry: path.resolve(__dirname, 'src/index.ts'),
 			name: 'react-markdown-editor',
 			fileName: (format) => `react-markdown-editor.${format}.js`,
 		},
